25c4d3664d5c7b154913197baf372631fb5a31b0
Eugene <nemesises@live.com>
2026-04-09 20:19:37 +0300

    Added app service and dto's for product categories

---
+++ src/SixThreeTwo_shop.Application/Categories/Mappings/ProductCategoryMapProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SixThreeTwo_shop.Categories.Dto;
+using SixThreeTwo_shop.Products;
+
+namespace SixThreeTwo_shop.Categories.Mappings;
+
+public class ProductCategoryMapProfile : Profile
+{
+  public ProductCategoryMapProfile()
+  {
+    CreateMap<ProductCategory, CategoryDto>();
+    
+    CreateMap<CategoryDto, ProductCategory>();
+  }
+}
