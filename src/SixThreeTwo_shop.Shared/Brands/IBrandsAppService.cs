c6da8adebc40b83a476db258d01664728424b560
Eugene <nemesises@live.com>
2026-07-15 19:09:34 +0300

    Added app service to manage brands

---
+++ src/SixThreeTwo_shop.Shared/Brands/IBrandsAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using SixThreeTwo_shop.Shared.Brands.Dto;
+
+namespace SixThreeTwo_shop.Shared.Brands;
+
+public interface IBrandsAppService : IApplicationService
+{
+  Task<List<BrandDto>> GetAllAsync();
+
+  Task<BrandDto> GetByIdAsync(int id);
+
+  Task DeleteBrand(int id);
+
+  Task<int> CreateEditBrand(BrandDto brandDto);
+}
